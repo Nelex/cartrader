6609c3db4d15848ae9265f3bae2ab29d090331aa
nelex <nelexis@gmail.com>
2025-02-20 00:15:48 +0200

    add NuxtImage component for replace tag img

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -8,7 +8,7 @@ const props = defineProps({
   @click="navigateTo(`/car/${car.name}-${car.id}`)"
   >
     <div class="flex h-full">
-      <img :src="car.url" alt="car 1" class="w-[300px] h-full">
+      <NuxtImg :src="car.url" alt="car 1" class="w-[300px] h-full" />
       <div class="p-4 flex flex-col">
         <div>
           <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
--- components/Car/Detail/Hero.vue
+++ components/Car/Detail/Hero.vue
@@ -5,7 +5,7 @@ const props = defineProps({
 </script>
 <template>
   <div class="mt-10">
-    <img :src="car.url" alt="full car photo" class="w-full">
+    <NuxtImg :src="car.url" alt="full car photo" class="w-full" />
     <h1 class="mt-10 text-4xl">{{car.name}}</h1>
     <div class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between">
       <div class="flex">
